da3b1ff87a3348c2ca60556a8692e7101e578530
Yuval Ron <yuvr99@gmail.com>
2023-08-17 20:45:52 +0300

    moved data folder to root

--- src/utils/getIgnoredUsers.ts
+++ src/utils/getIgnoredUsers.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export function getIgnoredUsers() {
-	const ignoredUsersFilePath = path.resolve(__dirname, '../data/ignoredUsers.json');
+	const ignoredUsersFilePath = path.resolve(__dirname, '../../data/ignoredUsers.json');
 	const ignoredUsers = fs.readFileSync(ignoredUsersFilePath, 'utf-8');
 	return JSON.parse(ignoredUsers);
 }
