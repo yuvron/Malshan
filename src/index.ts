27e774a402e17502159a7912e6006a3a87cd063c
Yuval Ron <yuvr99@gmail.com>
2023-11-04 13:38:26 +0200

    update versions and add logger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import DiscordClient from './clients/discord';
 import WhatsappClient from './clients/whatsapp';
+import fs from 'fs';
+
+if (!fs.existsSync('./logs')) {
+	fs.mkdirSync('./logs');
+}
+const logStream = fs.createWriteStream(
+	`./logs/${new Date()
+		.toISOString()
+		.slice(0, 16)
+		.replace(/[-T:.]/g, '_')}.log`,
+	{ flags: 'a' }
+);
+process.stdout.write = logStream.write.bind(logStream);
+process.stderr.write = logStream.write.bind(logStream);
 
 const whatsapp = new WhatsappClient();
 const discord = new DiscordClient();
